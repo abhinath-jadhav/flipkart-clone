3c93f30eecf3d50ee5b5eff46dcf06208cd1a413
abhinath-jadhav <137531738+abhinath-jadhav@users.noreply.github.com>
2023-11-16 12:31:18 +0530

    Update

--- src/components/SearchResult/SearchBody.tsx
+++ src/components/SearchResult/SearchBody.tsx
@@ -1,44 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-interface User {
-  name: string;
-  id: string;
-  contact: string;
-  email1: string;
-}
-
 const SearchBody = ({ className }: { className: string }) => {
-  const user1 = {
-    name: "",
-    id: "",
-    contact: "",
-    email1: "",
-  };
-  const [user, setUser] = useState<User>(user1);
-
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch("http://localhost:8081/flipkart/1", {
-        method: "POST",
-      });
-      const user = await res.json();
-      setUser(user);
-    };
-
-    console.log(user);
-    getData();
-  }, []);
-
-  console.log(user);
-
-  return (
-    <div className={`${className} bg-white`}>
-      <h1>{user?.name}</h1>
-      <h1>{user?.id}</h1>
-      <h1>{user?.contact}</h1>
-      <h1>{user.email1}</h1>
-    </div>
-  );
+  return <div className={`${className} bg-white`}></div>;
 };
 
 export default SearchBody;
--- test.js
+++
@@ -1,6 +0,0 @@
-var a = 10;
-
-for (let i = 0; i < 10; i++) {
-  console.log(a);
-  a += 6;
-}
